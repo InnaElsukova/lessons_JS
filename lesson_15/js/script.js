977b0fef89c6718fc165615d2f15998495e4df98
Inna Elsukova <inna.elsukova@gmail.com>
2024-04-15 20:20:52 +0300

    Lesson 15

---
+++ lesson_15/js/script.js
@@ -0,0 +1,17 @@
+function log(msg) {
+    let logEl = document.getElementById("log")
+    if (!logEl) {
+        logEl = document.createElement("pre")
+        logEl.id = "log"
+        document.body.appendChild(logEl)
+    }
+    logEl.innerText += msg + "\n"
+}
+
+function generateRandomArray(min = 1, max = 10000, count = 10) {
+    let randArray = []
+    for (let i = 0; i < count; i++) {
+        randArray.push(min + Math.floor(Math.random() * (max - min + 1)))
+    }
+    return randArray
+}
